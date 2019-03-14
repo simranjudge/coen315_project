3073b0b2670dc86bf0f7b9c6e2f6b636ca3e7362
Michaela Vuklisevicova <m.vuklisevicova@gmail.com>
2019-03-14 11:14:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -32,11 +32,53 @@ $('.action').click(function() {
     }
 });
 
+/* receive data from the server API and update the UI */
 async function getResults() {
-	const result = await
-	fetch('http://instaplants.sim315.com/plants');
-	const data = await result.json();
-	console.log(data);
-}
+    try {
+        const result = await
+        fetch('http://instaplants.sim315.com/plants');
+        const data = await result.json();
+        console.log(data);
+
+        // 1) update water level and display corresponding arrow
+        const waterLevel = data.waterLevels[0];
+        
+            // hide the current displayed arrow
+            document.querySelectorAll('.arrow').addClass('hide');
+            if(waterLevel >= 80) {
+                // show top (green) arrow
+                document.getElementById('#arrow-0').removeClass('hide');
+            }
+            else if(waterLevel >= 40) {
+                document.getElementById('#arrow-1').removeClass('hide');
+            }
+            else {
+                document.getElementById('#arrow-2').removeClass('hide');
+            }
+        
+        // 2) loop through each station and update all the values
+        for(let i = 0; i < 3; i++) {
+            
+            // update hours of light received
+            document.getElementById(`#lightInfo-${i}`).value = "9";
+            
+            // update state of the light bulb
+            if(data.lightBulbs[i] == 1) {
+                document.getElementById(`#bulbState-${i}`).value = "ON";
+            }
+            else {
+                document.getElementById(`#bulbState-${i}`).value = "OFF";
+            }
+            
+            // update moisture
+            document.getElementById(`#moistureInf-${i}`).value = `${data.moistureLevels[i]} %`;
+            
+            // plantTubes? lightSensors?
+        }
+    }
+    catch(error) {
+        alert(error);
+    }
+};
 
 getResults();
